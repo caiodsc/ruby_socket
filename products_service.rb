b5ddc85c525125db8e9679198413b3f67bdde92c
Caio <caio.dscamara@gmail.com>
2017-11-30 23:18:07 -0400

    Adicionando o Thin.

--- products_service.rb
+++ products_service.rb
@@ -52,23 +52,27 @@ class App < Sinatra::Base
   set :server, 'thin'
   set :sockets, []
 
+  Faye::WebSocket.load_adapter('thin')
+
   get '/' do
-    if !request.websocket?
-      erb :index
-    else
-      request.websocket do |ws|
-        ws.onopen do
-          ws.send("Hello World!")
-          settings.sockets << ws
-        end
-        ws.onmessage do |msg|
-          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-        end
-        ws.onclose do
-          warn("websocket closed")
-          settings.sockets.delete(ws)
-        end
+    if Faye::WebSocket.websocket?(request.env)
+      ws = Faye::WebSocket.new(request.env)
+
+      ws.on(:open) do |event|
+        puts 'On Open'
       end
+
+      ws.on(:message) do |msg|
+        ws.send(msg.data.reverse)  # Reverse and reply
+      end
+
+      ws.on(:close) do |event|
+        puts 'On Close'
+      end
+
+      ws.rack_response
+    else
+      erb :index
     end
   end
 
